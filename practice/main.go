ab0c5145ceccf31de8f98952c12ad104937231db
gagandeep.singh@logrhythm.com <gagandeep.singh5@globallogic.com>
2022-01-16 17:16:31 +0530

    adding changes

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 43.0
+	p := &anInt
+	fmt.Println("Value of p:", *p)
+
+	value1 := 42.43
+	pointer1 := &value1
+	fmt.Println("Value 1:", *pointer1)
+
+	*pointer1 = *pointer1 / 31
+	fmt.Println("Pointer 1:", *pointer1)
+	fmt.Println("Value 1:", value1)
+
+	*pointer1 = anInt
+	fmt.Println("new value of pointer 1:", *pointer1)
+	fmt.Println("Memory address of pointer 1:", pointer1)
+	fmt.Println("Memory address of anInt:", &anInt)
 }
